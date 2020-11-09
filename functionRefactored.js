8ca15c934ae0b5f38de1a97ff2be7be8974fbf5e
pmiks <e.pmiks@yandex.ru>
2020-11-09 20:28:28 +0400

    first commit

---
+++ functionRefactored.js
@@ -0,0 +1,11 @@
+function funcR( s , a , b ) {
+    if (!s.length) return - 2 ;
+    let i = s.length ;
+    let aIndex = bIndex = -1;
+    while ((aIndex == - 1) && (bIndex == - 1) && (--i > 0)) {
+        if (s[i] == a) aIndex = i;
+        if (s[i] == b) bIndex = i;
+    }
+    return  aIndex != - 1 || bIndex != - 1? Math. max (aIndex, bIndex):-1
+}
+
